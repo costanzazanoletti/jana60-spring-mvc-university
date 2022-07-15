9d62fbe0e10c8e9177b407b6af79748f149df56d
costanzazanoletti <zanoletti.cos@gmail.com>
2022-07-15 11:49:07 +0200

    correzione  Spring MVC University

---
+++ src/main/java/jana60/Jana60MvcUniversityApplication.java
@@ -0,0 +1,13 @@
+package jana60;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jana60MvcUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jana60MvcUniversityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/jana60/controller/DepartmentController.java
@@ -0,0 +1,27 @@
+package jana60.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import jana60.repository.DepartmentRepository;
+
+@Controller
+@RequestMapping("/")
+public class DepartmentController {
+
+  @Autowired
+  private DepartmentRepository repo;
+
+
+  @GetMapping
+  public String departments(Model model) {
+    // List<Department> list = (List<Department>) repo.findAll();
+    // for (Department d : list) {
+    // System.out.println(d.getId() + " - " + d.getName());
+    // }
+    model.addAttribute("departmentList", repo.findAll());
+    return "departments";
+  }
+}
